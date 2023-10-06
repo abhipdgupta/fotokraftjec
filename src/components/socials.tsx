aebb85fb9cb299865a162f272a8c4505cd56afbe
Abhishek Prasad Gupta <abhipdgupta007@gmail.com>
2023-10-07 01:03:54 +0530

    changes

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -6,17 +6,17 @@ export default function Socials() {
   return (
     <>
       <span className=" m-4 outline outline-2 outline-white rounded-full cursor-pointer   ">
-        <Link target="_blank" href={"https://instagram.com"}>
+        <Link target="_blank" href={"https://instagram.com/fotokraftjec_official?igshid=MWZjMTM2ODFkZg=="}>
           <InstagramIcon className="w-6 h-6 m-3 hover:scale-125 text-white hover:text-pink-400 transition-all" />
         </Link>
       </span>
       <span className=" m-4 outline  outline-2 outline-white rounded-full cursor-pointer">
-        <Link target="_blank" href={"https://youtube.com"}>
+        <Link target="_blank" href={"https://www.youtube.com/@fotokraftjec3309/"}>
           <YoutubeIcon className="w-6 h-6 m-3 hover:scale-125 text-white hover:text-red-500 transition-all" />
         </Link>
       </span>
       <span className=" m-4 outline outline-2 outline-white rounded-full cursor-pointer ">
-        <Link target="_blank" href={"https://facebook.com"}>
+        <Link target="_blank" href={"https://www.facebook.com/Photographyclub.JEC/"}>
           <FacebookIcon className="w-6 h-6 m-3 hover:scale-125  text-white hover:text-blue-500 transition-all" />
         </Link>
       </span>
