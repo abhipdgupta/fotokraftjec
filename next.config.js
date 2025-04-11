72e5ffbd5c6f03b2c1f6e6a0cb83bf7e8a479547
Abhishek Prasad Gupta <abhipdgupt007@gmail.com>
2025-04-12 02:12:11 +0530

    add gh page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    basePath: '/fotokraftjec',
+    images: {
+      unoptimized: true
+    },
+}
 
 module.exports = nextConfig
